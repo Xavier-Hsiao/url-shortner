ebbd1b2b7a1d14653414c8277f4b8af28c255252
Xavier-Hsiao <tim88776@gmail.com>
2021-11-09 22:48:19 +0800

    feat: include handlebars

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars') 
 const port = 3000
 
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('URL SHORTNER!!')
+  res.render('index')
 })
 
 app.listen(port, () => {
