bf07075293449b6671edaa1d423dd9e6888568e3
Xavier-Hsiao <tim88776@gmail.com>
2021-11-10 21:52:19 +0800

    feat: include Mongoose

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars') 
+const mongoose = require('mongoose')
 const port = 3000
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
+
+mongoose.connect('mongodb://localhost/url-shortner')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('OH NO! MONGODB ERROR!!!')
+})
+
+db.once('open', () => {
+  console.log('DB OPEN!!')
+})
 
 app.get('/', (req, res) => {
   res.render('index')
